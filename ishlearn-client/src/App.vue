8d03b8bcf6d5fd93d29197626c810876dac05bf9
root <root@DESKTOP-BKDHN8A.localdomain>
2022-05-23 23:46:19 +0200

    bad navbar with darkmode toggle button

--- ishlearn-client/src/App.vue
+++ ishlearn-client/src/App.vue
@@ -1,8 +1,10 @@
 <template>
+<div class="usual-color">
+  <NavBar />
   <div class="wrapper container">
-    <NavBar />
     <router-view />
   </div>
+</div>
 </template>
 
 <script setup lang="ts">
@@ -12,6 +14,12 @@ import NavBar from '@/components/NavBar.vue'
 // TODO: Example, implement actual code and remove following two lines
 const staticNr = ref(0)
 console.log(staticNr)
+
+// Code
+const storedTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+if (storedTheme) {
+  document.documentElement.setAttribute('data-theme', storedTheme)
+}
 </script>
 
 <style scoped>
