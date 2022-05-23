a279ffccb47b2f57cd3162e154f2f218978f99a6
root <root@DESKTOP-BKDHN8A.localdomain>
2022-05-23 20:25:34 +0200

    Dummer Wechsel von Branches

--- ishlearn-client/src/App.vue
+++ ishlearn-client/src/App.vue
@@ -1,42 +1,19 @@
 <template>
   <div class="wrapper container">
-    <nav>
-      <router-link :to="{ name: 'Home' }">Home</router-link> |
-      <router-link :to="{ name: 'About' }">About</router-link>
-    </nav>
+    <NavBar />
     <router-view />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import NavBar from '@/components/NavBar.vue'
 
 // TODO: Example, implement actual code and remove following two lines
 const staticNr = ref(0)
 console.log(staticNr)
 </script>
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+<style scoped>
 
-#app {
-  font-family: 'Roboto', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ ishlearn-client/src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <nav class="navbar navbar-expand-sm bg-light">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <button @click="count++" class="btn btn-secondary">
+            You clicked me {{ count }} times.
+          </button>
+        </li>
+        <li class="nav-item"><router-link :to="{ name: 'Home' }">Home</router-link></li>
+        <li class="nav-item"><router-link :to="{ name: 'About' }">About</router-link></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
