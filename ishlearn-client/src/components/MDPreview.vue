b32eb25c0e374389806643cb6567cb7e7115d7c7
root <root@DESKTOP-BKDHN8A.localdomain>
2022-06-23 19:25:09 +0200

    Einige Änderungen hin zu einem Add Projekt Formular

---
+++ ishlearn-client/src/components/MDPreview.vue
@@ -0,0 +1,60 @@
+<!-- GenericInput.vue -->
+<script setup lang="ts">
+import { defineProps, ref, watch } from 'vue'
+
+const markdownOption = {
+  bold: true,
+  italic: true,
+  header: true,
+  underline: true,
+  strikethrough: true,
+  mark: true,
+  superscript: true,
+  subscript: true,
+  quote: true,
+  ol: true,
+  ul: true,
+  link: true,
+  imagelink: false, // changed
+  code: true,
+  table: true,
+  fullscreen: true,
+  readmodel: true,
+  htmlcode: false, // changed
+  help: true,
+  /* 1.3.5 */
+  undo: true,
+  redo: true,
+  trash: false, // changed
+  save: false, // changed
+  /* 1.4.2 */
+  navigation: true,
+  /* 2.1.8 */
+  alignleft: true,
+  aligncenter: true,
+  alignright: true,
+  /* 2.2.1 */
+  subfield: true,
+  preview: true,
+}
+
+const props = defineProps(['textToDisplay'])
+// props.textToDisplay verändert sich, falls das Attribut im Eltern Component verändert wird.
+// mdtext ist jedoch Konstant!
+const mdtext = ref(props.textToDisplay)
+</script>
+
+<template>
+  <div>
+    <mavon-editor
+      v-model="mdtext"
+      language="de"
+      :toolbars="markdownOption"
+      v-bind:subfield="false"
+      defaultOpen="preview"
+      v-bind:toolbarsFlag="false"
+    ></mavon-editor>
+  </div>
+</template>
+
+<style scoped></style>
--- ishlearn-client/src/views/ProjectAdd.vue
+++ ishlearn-client/src/views/ProjectAdd.vue
@@ -1,28 +1,74 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import MDEditor from '@/components/MDEditor.vue'
+import MDPreview from '@/components/MDPreview.vue'
+import GenericInput from '@/components/GenericInput.vue'
+import { GenericInputs } from '@/types/GenericInputData'
 
-const mdtext = ref('')
+const inputs: GenericInputs<string | boolean> = {
+  title: {
+    value: ref(''),
+    type: 'text',
+    label: 'Wähle einen Titel für dein Project',
+    id: 'title',
+    name: 'title',
+    mandatory: true,
+    placeholder: '',
+  },
+  visibility: {
+    value: ref(false),
+    type: 'checkbox',
+    label: 'Soll das Projekt öffentlich sein?',
+    id: 'visibility',
+    name: 'visibility',
+    mandatory: true,
+    placeholder: '',
+  },
+}
+
+const mdtext = ref(`# Hier ist eine Test MD
+Lorem Ipsum xx.xx
+Datei von mir geschrieben, damit du was hast.
+
+----
+Hallo xD xD`)
+
+const onSubmit = (event) => {
+  console.log('Submitted')
+
+  // alle Eingaben auf Richtigkeit überprüfen.
+
+  console.log(event)
+}
 </script>
 
 <template>
   <div>
-    <h1>Hier kannst du dein Projekt hinzufügen.</h1>
+    <div class="container forms-small p-3">
+      <h1>Hier kannst du dein Projekt hinzufügen.</h1>
 
-    <p>
-      <!--
-      <form action="">
-        <label for="title"> Titel <input type="text" v-model="title" id="title" /></label>
-        <label for="thema">Thema</label><input type="text" name="thema" id="thema" />
+      <p>Fülle bitte alle notwendigen Felder aus.</p>
+
+      <form @submit.prevent="onSubmit" class="form-input-group">
+        <GenericInput
+          v-for="input in inputs"
+          :key="input.id"
+          v-model="input.value.value"
+          :inputProps="input"
+        />
+        <p>Projektbeschreibung</p>
+        <MDEditor v-model="mdtext" />
+
+        <input type="submit" value="Projekt erstellen" class="btn btn-success" />
       </form>
-      -->
-    </p>
+
+      <p class="tiny-font">(*) sind Pflichtfelder.</p>
+    </div>
 
     <button class="btn btn-primary">Dieser Knopft Submitted nichts</button>
 
-    <p>TEST</p>
-
-    <br /><br /><br />
-
-    <mavon-editor v-model="mdtext" language="de" />
+    <p>{{ mdtext }}</p>
+    <MDPreview :text-to-display="mdtext"></MDPreview>
+    <br />
   </div>
 </template>
