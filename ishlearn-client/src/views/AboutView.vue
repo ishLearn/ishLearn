17449c2b950e24d1dc07643a76129851b612f382
root <root@DESKTOP-BKDHN8A.localdomain>
2022-06-23 08:05:20 +0200

    Textänderungen auf einigen Seiten.

--- ishlearn-client/src/views/AboutView.vue
+++ ishlearn-client/src/views/AboutView.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="">
     <div class="about">
-      <h2>This is an about page</h2>
-      <p>Hier kommt noch ein weiterer Absatz.</p>
+      <h2>About</h2>
+      <h3>Die Idee</h3>
+      <p>
+        ISHLearn ist die neue Plattform für Lehrer und Schüler, Abgaben und Projekte zu
+        koordinieren. Neben der Abgabe verschiedenster Dateiformate und unterschiedlichen
+        Sichtbarkeitsstufen ermöglicht ISHLearn es den Lehrern, bequem Feedback zu den Projekten zu
+        geben. Anschließend können die Projekte für andere Schüler sichtbar gemacht werden, sodass
+        die viele Arbeit, die in das Erstellen dieser Projekte gesteckt wird, nicht auf einem
+        Dateienfriedhof landet, sondern auch zukünftig von anderen Schülern aus Interesse oder für
+        das Lernen z.B. vor Klausuren genutzt werden kann.
+      </p>
+      <h3>Das Team</h3>
+      <p>
+        Wir heißen Sebastian Thomas und Johann Gaulke und sind zwei Schüler an der Internatsschule
+        Schloss Hansenberg. Wir sind Informatik begeistert und wollen mit diesem Projekt für den
+        Informatik LK die Welt verbessern.
+      </p>
     </div>
-    <div class="">Some Text hier to be cool.</div>
   </div>
 </template>
