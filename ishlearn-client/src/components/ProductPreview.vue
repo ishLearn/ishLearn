7050202b231966a37c7a8e7c5fead485650ea2f8
root <root@DESKTOP-BKDHN8A.localdomain>
2022-07-06 22:11:15 +0200

    Searching for products is now possible

--- ishlearn-client/src/components/ProductPreview.vue
+++ ishlearn-client/src/components/ProductPreview.vue
@@ -1,17 +1,80 @@
 <!-- ProductPreview.vue -->
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import { defineProps, ref, Ref } from 'vue'
+import { formatDate } from '@/util/dateUtils'
+import { User } from '@/types/Users'
+import { getUser } from '@/util/getUser'
 
-defineProps(['project'])
+const props = defineProps(['project'])
+const creator: Ref<User | null> = ref(null)
+
+getUser(creator, props.project.createdBy)
 </script>
 
 <template>
-  <div class="media border p-3 m-2">
-    {{ project.title }}
-    <p>
-      <router-link :to="{ name: 'ViewProject', params: { id: project.id } }"
-        >Anschauen!</router-link
+  <div class="media box-background rounded shadow m-2">
+    <div class="heading">
+      <h3>{{ project.title }}</h3>
+    </div>
+    <div class="trenner">ein Projekt von</div>
+    <div class="content">
+      <div class="creator">
+        <router-link
+          v-if="creator"
+          class="creator"
+          :to="{ name: 'UserDetail', params: { id: creator.id } }"
+          >{{ creator.firstName }}</router-link
+        >
+      </div>
+      <small class="text-muted"
+        >Erstellt am <br />
+        {{ formatDate(project.updatedDate) }}</small
       >
-    </p>
+
+      <div style="visibility: hidden">
+        <router-link :to="{ name: 'ViewProject', params: { id: project.id } }">
+          <button class="btn btn-secondary">Ansehen</button></router-link
+        >
+        <small>Bearbeitet: {{ formatDate(project.updatedDate) }}</small>
+      </div>
+
+      <div class="bottom-note text-muted p-2">
+        <router-link :to="{ name: 'ViewProject', params: { id: project.id } }">
+          <button class="btn btn-secondary">Ansehen</button></router-link
+        >
+        <small>Bearbeitet: {{ formatDate(project.updatedDate) }}</small>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.media {
+  position: relative;
+}
+.creator {
+  text-decoration: none;
+}
+.trenner {
+  width: 100%;
+  min-height: 1rem;
+  background-color: rgba(0, 0, 0, 0.2);
+}
+.content {
+  padding: 0rem 1rem 1rem 1rem;
+}
+button {
+  padding: 10px;
+  width: 100%;
+  border: 0px;
+  background-color: var(--orange);
+}
+.bottom-note {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  vertical-align: bottom;
+  width: 100%;
+  text-align: center;
+}
+</style>
--- ishlearn-client/src/views/ProjectDetail.vue
+++ ishlearn-client/src/views/ProjectDetail.vue
@@ -76,7 +76,7 @@ watch(project, () => descriptionUpdate.value++)
             v-if="editPermission"
             :to="{ name: 'UpdateProject', params: { id: project.id } }"
           >
-            <button class="btn btn-sm btn-secondary button edit-button">Bearbeiten</button>
+            <button class="btn btn-sm btn-secondary edit-button">Bearbeiten</button>
           </router-link>
         </h4>
 
